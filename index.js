81889d5220064a847f2060391cb7a36c4e210e6c
Jaun Nothnagel <jaunn21@gmail.com>
2024-04-12 12:57:45 +0200

    feature testing

--- index.js
+++ index.js
@@ -9,46 +9,33 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
- const danaCoordinates = "34.1962,22.0536"
-
-
-const options = {
-  method: 'GET',
-  url: 'https://weatherapi-com.p.rapidapi.com/current.json',
-  params: {q: danaCoordinates},
-  headers: {
-    'X-RapidAPI-Key': 'c93cedb6bfmshdbd8f5598e8443cp18397ejsn066c2e325810',
-    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-  }
-};
-
-let lat ="";
-let lon ="";
-
-app.get("/", async(req,res) =>{
+app.get("/", (req,res) =>{
    res.render("weatherData.ejs");
-        //  
+         
 });
 
-app.post("/", async(req,res) =>{
-  const options = {
-    method: 'GET',
-    url: 'https://weatherapi-com.p.rapidapi.com/current.json',
-    params: {q: `${req.body.latittude},${req.body.longitude}`},
-    headers: {
-      'X-RapidAPI-Key': 'c93cedb6bfmshdbd8f5598e8443cp18397ejsn066c2e325810',
-      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-    }
-  };
+// app.post("/", async(req,res) =>{
+//   const options = {
+//     method: 'GET',
+//     url: 'https://weatherapi-com.p.rapidapi.com/current.json',
+//     params: {q: `${req.body.latittude},${req.body.longitude}`},
+//     headers: {
+//       'X-RapidAPI-Key': 'c93cedb6bfmshdbd8f5598e8443cp18397ejsn066c2e325810',
+//       'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+//     }
+//   };
   
-  try {
-    const response = await axios.request(options);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-  res.render("index.ejs",{wholeObj:jsObject})
-});
+//   try {
+//     const response = await axios.request(options);
+//     const responseString = JSON.stringify(response.data);
+//     const jsObject = JSON.parse(responseString);
+//     res.render("index.ejs",{wholeObj:jsObject})
+    
+//   } catch (error) {
+//     console.error(error);
+//   }
+  
+// });
 
 
 
